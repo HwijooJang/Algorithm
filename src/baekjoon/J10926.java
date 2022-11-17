ecd49ec6d14b377ba4a6be48f8e888173a1ec4f5
HwijooJang <jhj9469@naver.com>
2022-11-17 21:29:05 +0900

    백준 2일차

---
+++ src/baekjoon/J10926.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class J10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a;
+        a = sc.next();
+
+        System.out.println(a + "?!");
+
+    }
+
+}
